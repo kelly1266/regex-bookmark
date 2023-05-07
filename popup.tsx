b8794dd6e577e824861efa8c8358d4a02cf91a3e
kelly1266 <t.kelly79285@gmail.com>
2023-05-07 18:42:49 -0400

    start building out popup page

--- popup.tsx
+++ popup.tsx
@@ -1,28 +1,80 @@
-import { useState } from "react"
+import "react"
+import { UserOutlined, SettingOutlined } from "@ant-design/icons"
+import { Row, Col, Typography, Space, Button } from "antd"
+import type { Bookmark } from "./types"
+import { getBookmark } from "~utils"
 
-function IndexPopup() {
-  const [data, setData] = useState("")
+const { Title } = Typography;
 
+function openBookmarkSettings(){
+
+}
+
+function bookmarksList(){
+  // chrome.storage.local.get("configObject", (data)=>{
+  //   data.configObject.Bookmarks.map((bookmark: Bookmark) => {
+  //     bookmarkTile(bookmark.title)
+  //   })
+  // })
+  const books = ["Bookmark 1", "Bookmark 2", "Bookmark 3"]
+  const booksMapped = books.map(id=>{
+    return bookmarkTile(id)
+  })
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <>
+      {booksMapped}
+    </>
   )
 }
 
-export default IndexPopup
+function bookmarkTile(id: string){
+  const bookmark = getBookmark(id)
+  return (
+    <Space direction="horizontal" wrap={false}>
+      <Button >
+        {id}
+      </Button>
+      <Button>
+        <SettingOutlined />
+      </Button>
+    </Space>
+  )
+}
+
+function addABookmark(){
+  function openNewBookmarkTab() {
+    window.open("tabs/newBookmark.html")
+  }
+
+  return (
+    <Space direction="horizontal" wrap={false} onClick={ openNewBookmarkTab }>
+      <Button >
+        Add a Bookmark
+      </Button>
+    </Space>
+  )
+}
+
+function popUpHeader(){
+  function openAccountSettings(){
+    window.open("tabs/accountSettings.html")
+  }
+
+  return (
+    <Space direction="horizontal" wrap={false} >
+      <h2>Bookmarks</h2>
+      <UserOutlined style={{ fontSize:"24px" }} onClick={openAccountSettings}/>
+    </Space>
+  )
+}
+
+
+export default function IndexPopup() {
+  return (
+    <div>
+      {popUpHeader()}
+      {bookmarksList()}
+      {addABookmark()}
+    </div>
+  )
+}
